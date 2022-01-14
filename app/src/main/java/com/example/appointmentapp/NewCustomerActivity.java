3887ca8e8e8a76eea88e4d359d3ed415ca405bdf
JESSE <jessejzee@gmail.com>
2022-01-14 09:28:08 +0300

    Started working on getting data for spinner drop downlist, pending adapter

--- app/src/main/java/com/example/appointmentapp/NewCustomerActivity.java
+++ app/src/main/java/com/example/appointmentapp/NewCustomerActivity.java
@@ -3,11 +3,11 @@ package com.example.appointmentapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -20,7 +20,6 @@ import com.android.volley.Response;
 import com.android.volley.ServerError;
 import com.android.volley.TimeoutError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.JsonRequest;
 
 
 import org.json.JSONException;
@@ -56,6 +55,76 @@ Button btnReg;
             }
         });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        try {
+            fillspward();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void fillspward() throws JSONException
+    {
+        JSONObject jsonObject=new JSONObject();
+        JsonObjectRequest Newrequest = new JsonObjectRequest(
+
+                Request.Method.GET,URLS.getmaindepat,jsonObject,response -> {
+            try{
+                    String Maindept = response.toString();
+
+//                SpinnerAdapter adapter;
+//                SpWard.setAdapter(adapter);
+                }
+                catch (Exception e){
+                    Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+        }, volleyError -> {
+//                    progressDialog.dismiss();
+            String message = null;
+            if (volleyError instanceof NetworkError) {
+                Toast.makeText(this, " Cannot connect to Internet...Please check your connection!", Toast.LENGTH_LONG).show();
+            } else if (volleyError instanceof ServerError) {
+                message = "The server could not be found. Please try again after some time!!";
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+            } else if (volleyError instanceof AuthFailureError) {
+                message = "Authentication Failed...Please check your Permissions!";
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+            } else if (volleyError instanceof ParseError) {
+                message = "Parsing error! Please try again after some time!!";
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+            } else if (volleyError instanceof NoConnectionError) {
+                message = "Cannot connect to Internet...Please check your connection!";
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+            } else if (volleyError instanceof TimeoutError) {
+                message = "Connection TimeOut! Please check your internet connection.";
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), volleyError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+
+
+            @Override
+            protected Response parseNetworkResponse(NetworkResponse response) {
+                return null;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+//               User user = SharedPrefManager.getInstance(getApplicationContext()).getToken();
+//                String Token = user.getJwtToken();
+//                params.put("Authorization", "Bearer " + Token);
+                return params;
+            }
+
+        };
+//        Mysingleton.getInstance(getApplicationContext()).addToRequestQueue(request);
+    }
+
 
     public void register() throws JSONException {
         JSONObject jsonObject=new JSONObject();
--- app/src/main/java/com/example/appointmentapp/URLS.java
+++ app/src/main/java/com/example/appointmentapp/URLS.java
@@ -2,6 +2,7 @@ package com.example.appointmentapp;
 
 public class URLS {
     public static final String URL_SEARCHMOTORBIKE = "";
-    public static final String REGISTER = "http://172.18.128.1/newcustomer";
-    public static final String NewMAindepartment = "http://172.18.128.1/newmaindepartment";
+    public static final String REGISTER = "http://192.168.0.161/newcustomer";
+    public static final String NewMAindepartment = "http://192.168.0.161/newmaindepartment";
+    public static final String getmaindepat = "http://192.168.0.161/newmaindepartment";
 }
